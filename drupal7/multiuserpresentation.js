e7e4870c4d2ebafc4f681cc8ea4609c0a8f2c286
jonny <dekan@gaenseklein.net>
2020-07-24 06:27:59 +0200

    changed join-option from hash to search

--- drupal7/multiuserpresentation.js
+++ drupal7/multiuserpresentation.js
@@ -116,7 +116,7 @@ ws.showDialog = function(){
     invitebutton.onclick = function(){copylink(this.innerHTML,this);};
     let ivlink = location.href;
     if(ivlink.indexOf("#")>-1)ivlink = ivlink.substring(0,ivlink.indexOf("#"));
-    ivlink+="#join";
+    ivlink+="?join";
     invitebutton.innerHTML = ivlink;
     content.appendChild(invitebutton);
     content.appendChild( document.createElement("hr"));
