15c5b9ed10cce11e9efefde4e35461b91ba1a501
manikani <marie-lux@live.de>
2020-09-15 07:59:55 +0200

    added new themes to replace the example ones

---
+++ themes/dark.js
@@ -0,0 +1,6 @@
+/*das ist ein beispiel-theme script. es tut nichts weiter, als festzulegen, dass es den namen dark trägt, ein basic-theme ist (dark.themetype = "css" und eine description, die beschreibt, was die user erwartet*/ 
+var dark = new Theme("dark"); //erstelle ein theme mit namen dark
+
+dark.themetype = "css"; //themetype ist css, also ein basis-theme und keine erweiterung
+dark.description = "a dark colored theme"; //beschreibung, die im tooltip angezeigt wird
+slidenote.addTheme(dark);// füge das theme der slidenote hinzu
