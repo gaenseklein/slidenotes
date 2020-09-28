6bbdae2ae87781efeedc57212835963555ba9395
jonny <dekan@gaenseklein.net>
2020-09-28 20:41:39 +0200

    tut

--- tutorials/tutorials.js
+++ tutorials/tutorials.js
@@ -1,6 +1,7 @@
 const tutoriallist = [
 {title: "welcome to slidenotes", filename: "welcome.md", url:"welcome"},
-{title: "chart tutorial", filename: "chart.md"},
-{title: "code tutorial", filename: "code.md"},
-{title: "image tutorial", filename: "image.md"},
+{title: "chart tutorial", filename: "chart.md", url: "chart"},
+{title: "code tutorial", filename: "code.md", url:"code"},
+{title: "image tutorial", filename: "image.md", url:"image"},
 ];
+
