64fd911afcd3b9c0c40a3ea6f2b3eeb51456909e
jonny <dekan@gaenseklein.net>
2020-10-06 06:18:15 +0200

    preview of themes with preview-image

--- themes/colorful.js
+++ themes/colorful.js
@@ -1,6 +1,6 @@
-/*das ist ein beispiel-theme script. es tut nichts weiter, als festzulegen, dass es den namen colorful trägt, ein basic-theme ist (colorful.themetype = "css" und eine description, die beschreibt, was die user erwartet*/ 
+/*das ist ein beispiel-theme script. es tut nichts weiter, als festzulegen, dass es den namen colorful trägt, ein basic-theme ist (colorful.themetype = "css" und eine description, die beschreibt, was die user erwartet*/
 var colorful = new Theme("colorful"); //erstelle ein theme mit namen colorful
-
+colorful.previewImage=true;
 colorful.themetype = "css"; //themetype ist css, also ein basis-theme und keine erweiterung
 colorful.description = "a colorful theme"; //beschreibung, die im tooltip angezeigt wird
 colorful.highlightTheme = "monokai-sublime"; //theme für highlight
